649d514f69cb0e0cba9973cee546face9d470331
Ivar Mahhonin <ivar.mahhonin@canter.fi>
2018-10-12 21:56:05 +0300

    basic solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgbModule, NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {HeartbeatComponent} from './heartbeat/heartbeat.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeartbeatComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    NgbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/heartbeat/heartbeat.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-heartbeat',
+  templateUrl: './heartbeat.component.html',
+  styleUrls: ['./heartbeat.component.scss']
+})
+export class HeartbeatComponent implements OnInit {
+  @Input() speed: number;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
